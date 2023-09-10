0fdfc87a90b67ac470fa11a70275ab59c67e96b7
Jozef Hudec <jojohudec27@gmail.com>
2023-09-10 12:21:33 +0200

    Make hero section responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ function HomePage() {
     <div>
       <section id="hero">
         <div
-          className="bg-[url('/assets/images/bartender_hero.jpg')] bg-cover bg-center border w-full h-[404px]"
+          className="bg-[url('/assets/images/bartender_hero.jpg')] bg-cover bg-center border w-full h-[25vh] sm:h-[47vh]"
           aria-hidden
         ></div>
 
-        <div className="flex flex-col gap-[42px] w-1/2 mx-auto mt-[64px]">
+        <div className="flex flex-col gap-[21px] sm:gap-[42px] w-11/12 md:w-3/4 xl:w-1/2 mx-auto mt-[32px] sm:mt-[64px]">
           <h1>Experience Culinary Bliss at Jackson's Bistro. Where Flavors Dance and Memories Are Savored.</h1>
 
           <Link href="" className="btn primary">
