44592d2361df9b49cb1643b3e5c63f95d6d7ca79
Jozef Hudec <jojohudec27@gmail.com>
2023-09-13 20:17:00 +0200

    Create menu page with hero section

---
+++ app/menu/page.tsx
@@ -0,0 +1,18 @@
+function MenuPage() {
+  return (
+    <div>
+      <section id="hero">
+        <div
+          className="bg-[url('/assets/images/menu_hero.jpg')] bg-cover bg-center border w-full h-[25vh] sm:h-[47vh]"
+          aria-hidden
+        ></div>
+
+        <h1 className="w-11/12 md:w-3/4 xl:w-1/2 mx-auto mt-[32px] sm:mt-[64px]">
+          Our Menu - Indulge in a Culinary Symphony. Each Dish Tells a Story. <span className="text-primary">Bon Appétit</span>.
+        </h1>
+      </section>
+    </div>
+  )
+}
+
+export default MenuPage
